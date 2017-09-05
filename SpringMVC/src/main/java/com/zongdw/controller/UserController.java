bacce73cd6f2b817f7fcd31a535f123a6f4e6961
hbyzzdw <hbyzzdw@163.com>
2017-09-05 17:06:05 +0800

    add springmvc

---
+++ SpringMVC/src/main/java/com/zongdw/controller/UserController.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.zongdw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zongdw.entity.User;
+
+/**
+ * 用户管理
+ * 
+ */
+@Controller
+public class UserController {
+
+    @RequestMapping("")
+    public String Create(Model model) {
+        return "create";
+    }
+
+    @RequestMapping("/save")
+    public String Save(@ModelAttribute("form") User user, Model model) { // user:视图层传给控制层的表单对象；model：控制层返回给视图层的对象
+        model.addAttribute("user", user);
+        return "detail";
+    }
+}
